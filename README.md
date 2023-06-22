1bd13d8635a79ad98e75418638778faba64256da
Bryan Velasco <bryanlvelasco@hotmail.com>
2023-06-21 23:51:55 -0600

    [FR-01]Initializing Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Re-learning how to use nuxt
+
+## TODO
+- add pages
+- add images
+- add tailwind (maybe bootstrap)
---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>I'm glad I can still at least do this</p>
+  </div>
+</template>
