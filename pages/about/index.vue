607c54afd2c6895f3bcb67937a9e9355f057432f
Bryan Velasco <bryanlvelasco@hotmail.com>
2023-07-19 23:13:52 -0600

    added about page H1

---
+++ pages/about/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <TheHeader />
+    </div>
+    <div>
+      <h1 class="m-5 text-center .bg-danger-subtle">
+        The About Page
+      </h1>
+    </div>
+  </div>
+</template>
